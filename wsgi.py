572831b401a09b0b1f8b4e58eb7be4e17d88007d
ZamIzzudin <azzamizzudinhasan@gmail.com>
2023-06-14 02:04:39 +0700

    setup wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,7 @@
 import os
 
-from routes.helper import app
+from app import app
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     port = int(os.environ.get('PORT', 8080))  
     app.run(port=port)
