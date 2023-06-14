09931176f00ea91109518afde51d892b4b6dd5ce
ZamIzzudin <azzamizzudinhasan@gmail.com>
2023-06-14 08:07:53 +0700

    disable predict

--- routes/helper.py
+++ routes/helper.py
@@ -9,35 +9,35 @@ endpoint_prefix = os.environ.get('ENDPOINT_PREFIX', '/')
 
 model = None
 
-@app.before_first_request
-def setup():
-    global model
-    model = load_model_from_gcs()
+# @app.before_first_request
+# def setup():
+#     global model
+#     model = load_model_from_gcs()
 
 @app.route(endpoint_prefix + '/', methods=['GET'])
 def default():
     return jsonify({'message': 'Welcome to the API'}),201
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    global model
-    if 'file' not in request.files:
-        return jsonify({'error': 'No file in the request.'}), 400
+# @app.route('/predict', methods=['POST'])
+# def predict():
+#     global model
+#     if 'file' not in request.files:
+#         return jsonify({'error': 'No file in the request.'}), 400
 
-    file = request.files['file']
-    num_images = 5
-    predict_category, predict_categories = load_and_predict(file,model)
+#     file = request.files['file']
+#     num_images = 5
+#     predict_category, predict_categories = load_and_predict(file,model)
 
-    if predict_category is None:
-        return jsonify({'error': 'Invalid image file.'}), 400
+#     if predict_category is None:
+#         return jsonify({'error': 'Invalid image file.'}), 400
     
-    recommended_images = get_recommended_images(predict_categories, num_images)
+#     recommended_images = get_recommended_images(predict_categories, num_images)
 
-    return jsonify({
-        'category_type' : int(predict_category),
-        'category_name' : predict_categories,
-        'path' : recommended_images
-    })
+#     return jsonify({
+#         'category_type' : int(predict_category),
+#         'category_name' : predict_categories,
+#         'path' : recommended_images
+    # })
 
 if __name__ == '__main__':
     # model = tf.keras.models.load_model('D:\Project\capstone3\model.h5')
