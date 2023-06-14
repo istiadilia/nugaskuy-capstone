dadc1263db2d1f10a1b402eaa5a73533169b6393
ZamIzzudin <azzamizzudinhasan@gmail.com>
2023-06-14 23:48:34 +0700

    change model to tflite

--- controllers/logic.py
+++ controllers/logic.py
@@ -9,7 +9,26 @@ from PIL import Image
 bucket_name = 'nugaskuy' 
 dataset_dir = 'dataset'
 
-local_model_path = 'model.h5'
+
+def load_and_run_tflite_model(model_path, input_data):
+    # Load the TFLite model
+    interpreter = tf.lite.Interpreter(model_path=model_path)
+    interpreter.allocate_tensors()
+
+    # Get input and output details
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+
+    # Set input data
+    interpreter.set_tensor(input_details[0]['index'], input_data)
+
+    # Run the interpreter
+    interpreter.invoke()
+
+    # Get the output results
+    output_data = interpreter.get_tensor(output_details[0]['index'])
+
+    return output_data
 
 def get_recommended_images(category, num_images):
     # Membuat objek storage client
@@ -37,7 +56,6 @@ def get_recommended_images(category, num_images):
 
 def load_and_predict(image):
     img_size = 224
-    model = tf.keras.models.load_model('gs://nugaskuy/model.h5')
 
     try:
         # Setup Image untuk kebutuhan prediksi
@@ -46,9 +64,10 @@ def load_and_predict(image):
         img = img.resize((img_size, img_size))
         img_array = np.array(img) / 255.0
         img_array = img_array[np.newaxis, ...]
+        img_array = img_array.astype(np.float32)
 
         # Melakukan prediksi
-        prediction = model.predict(img_array)
+        prediction = load_and_run_tflite_model('D:\Project\capstone3\modelv2.tflite',img_array)
         predicted_class = np.argmax(prediction)
 
         categories = ["logaritma", "spldv", "integral", "pertidaksamaan", "eksponen"]
