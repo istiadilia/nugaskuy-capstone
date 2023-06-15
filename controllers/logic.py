10e94b7251effa7c6df77b7b766737abfc483765
ZamIzzudin <88259831+ZamIzzudin@users.noreply.github.com>
2023-06-15 10:38:17 +0700

    try tflite

--- controllers/logic.py
+++ controllers/logic.py
@@ -10,25 +10,25 @@ bucket_name = 'nugaskuy'
 dataset_dir = 'dataset'
 
 
-# def load_and_run_tflite_model(model_path, input_data):
-#     # Load the TFLite model
-#     interpreter = tf.lite.Interpreter(model_path=model_path)
-#     interpreter.allocate_tensors()
+def load_and_run_tflite_model(input_data):
+    # Load the TFLite model
+    interpreter = tf.lite.Interpreter(model_path='gs://nugaskuy/modelv2.tflite')
+    interpreter.allocate_tensors()
 
-#     # Get input and output details
-#     input_details = interpreter.get_input_details()
-#     output_details = interpreter.get_output_details()
+    # Get input and output details
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
 
-#     # Set input data
-#     interpreter.set_tensor(input_details[0]['index'], input_data)
+    # Set input data
+    interpreter.set_tensor(input_details[0]['index'], input_data)
 
-#     # Run the interpreter
-#     interpreter.invoke()
+    # Run the interpreter
+    interpreter.invoke()
 
-#     # Get the output results
-#     output_data = interpreter.get_tensor(output_details[0]['index'])
+    # Get the output results
+    output_data = interpreter.get_tensor(output_details[0]['index'])
 
-#     return output_data
+    return output_data
 
 def get_recommended_images(category, num_images):
     # Membuat objek storage client
@@ -56,7 +56,7 @@ def get_recommended_images(category, num_images):
 
 def load_and_predict(image):
     img_size = 224
-    model = tf.keras.models.load_model('gs://nugaskuy/model.h5')
+#     model = tf.keras.models.load_model('gs://nugaskuy/model.h5')
 
     try:
         # Setup Image untuk kebutuhan prediksi
@@ -65,11 +65,11 @@ def load_and_predict(image):
         img = img.resize((img_size, img_size))
         img_array = np.array(img) / 255.0
         img_array = img_array[np.newaxis, ...]
-#         img_array = img_array.astype(np.float32)
+        img_array = img_array.astype(np.float32)
 
         # Melakukan prediksi
-        # prediction = load_and_run_tflite_model('gs://nugaskuy/modelv2.tflite',img_array)
-        prediction = model.predict(img_array)
+        prediction = load_and_run_tflite_model(img_array)
+#         prediction = model.predict(img_array)
         predicted_class = np.argmax(prediction)
 
         categories = ["logaritma", "spldv", "integral", "pertidaksamaan", "eksponen"]
